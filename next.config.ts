060d2ce34658859677bd994da3f1517a088ce5fc
Simon M. <git@simon.marache.net>
2025-01-01 13:26:46 +0000

    build nextjs artefacts locally

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  output: 'export',  // This tells Next.js to export static files
+  // Since we're not using images from external sources, we don't need an image loader
+  images: {
+    unoptimized: true
+  }
+}
 
-export default nextConfig;
+module.exports = nextConfig
